e6dd25aa931274b6980628d274e3293a47a1ec37
fripig <fripig@gmail.com>
2020-04-15 03:59:21 +0800

    Chart.vue start

--- resources/js/components/Chart.vue
+++ resources/js/components/Chart.vue
@@ -1,10 +1,30 @@
 <template>
-
+    <div>
+        <div class="chart" ref="chart"></div>
+    </div>
 </template>
 
 <script>
+    import {bb} from 'billboard.js'
     export default {
-        name: "Chart"
+        name: "Chart",
+        data(){
+          return {
+              chart:null
+          }
+        },
+        mounted() {
+            this.chart = bb.generate({
+                bindto: this.$refs.chart,
+                data: {
+                    type: "bar",
+                    columns: [
+                        ["data1", 30, 200, 100, 170, 150, 250],
+                        ["data2", 130, 100, 140, 35, 110, 50]
+                    ]
+                }
+            });
+        }
     }
 </script>
 
