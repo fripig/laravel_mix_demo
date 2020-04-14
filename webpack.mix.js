551ed5e0de9f23f14b999c0967dd6199c0d8e8b3
fripig <fripig@gmail.com>
2020-04-15 04:17:43 +0800

    mix.extract add jquery

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .extract(['vue','billboard.js'])
+    .extract(['vue','billboard.js','jquery'])
    .sass('resources/sass/app.scss', 'public/css');
